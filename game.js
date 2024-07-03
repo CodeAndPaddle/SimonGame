97bae235f55009d49f7038a91546fa5e8afc0db2
RonB <151724578+CodeAndPaddle@users.noreply.github.com>
2024-07-03 17:33:25 +0200

    Add files via upload

---
+++ game.js
@@ -0,0 +1,71 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+
+var gamePattern = [];
+var userClickedPattern=[];
+var firstTime = true;
+var level = 0 ;
+
+function nextSequence() {
+    var randomNumber = Math.floor(Math.random()*4);
+    var randomChosenColour = buttonColours[randomNumber];
+    gamePattern.push(randomChosenColour);
+    $("#" + randomChosenColour).fadeOut(100).fadeIn(100);
+    playSound(randomChosenColour);
+    level++;
+    $("#level-title").text("Level " +level);
+    
+    
+}
+
+$(".btn").on("click",function(){
+    var userChosenColour = $(this).attr("id")
+    userClickedPattern.push(userChosenColour);
+
+    playSound(userChosenColour);
+    animatePress(userChosenColour);
+
+    checkAnswer(userClickedPattern.length);
+})
+
+function playSound (color){
+    var audio = new Audio(color + ".mp3");
+    audio.play();
+}
+
+function animatePress(currentColour){
+    $("#" + currentColour).addClass("pressed");
+    setTimeout(function(){$("#" + currentColour).removeClass("pressed")}, 100);
+}
+
+$(document).on("keypress",function(){
+    if(firstTime){
+        setTimeout(function(){nextSequence();},200);
+        firstTime = false;
+    }
+});
+
+function checkAnswer(currentLevel){
+    if (gamePattern[currentLevel-1] === userClickedPattern[currentLevel-1]){
+        console.log("succes");
+    }
+    else{
+        var wrongAudio = new Audio("wrong.mp3");
+        wrongAudio.play();
+        $("body").addClass("game-over");
+        setTimeout(function(){$("body").removeClass("game-over")}, 200);
+        $("#level-title").text("Game Over, Press Any Key to Restart");
+        startOver();
+
+    }
+    if (currentLevel === level){
+        userClickedPattern = [];
+        setTimeout(function(){nextSequence();},1000);
+    }
+}
+
+function startOver(){
+    gamePattern = [];
+    userClickedPattern = [];
+    firstTime=true;
+    level = 0;
+}
